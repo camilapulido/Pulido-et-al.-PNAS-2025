e0e8aa25f13e1b05372fb1029b71db60a6ee9210
Camila Pulido <camilapulido@gmail.com>
2025-08-08 15:04:35 -0400

    Add files via upload

---
+++ ZeroGlucBlack_NewVersion.py
@@ -0,0 +1,70 @@
+from ij import IJ, ImagePlus
+from ij import WindowManager as WM 
+from ij.plugin.frame import RoiManager
+from ij.measure import Measurements, ResultsTable
+from ij.io import FileSaver
+import os
+import time
+import glob
+
+###### Setup Val ####
+xVal= 1650 ##2900 ##1250 ## x value to click in Get Avg
+yVal = 229 
+
+Waitfor = 1  # in seconds
+##################################################################
+##################################################################
+##################################################################
+##################################################################
+CRE = "WT\\"
+Culture = "Dopaminergic\\"
+sensor = "SynHylight"
+
+Date = 250422
+Cell = 2
+              
+#############################################
+
+FolderOUT = "D:\\Dropbox\\LABORATORY\\ANALYSIS\\"+Culture+CRE+"ZeroGlucose\\"+sensor+"\\2025\\"+str(Date)+"_C"+str(Cell)+"\\"
+FolderIN = "D:\\Dropbox\\LABORATORY\\DATA\\"+Culture+CRE+"ZeroGlucose\\"+sensor+"\\2025\\"+str(Date)+"\\C"+str(Cell)+"\\"
+
+if not os.path.exists(FolderOUT):
+	os.makedirs(FolderOUT)
+############		
+##################
+
+fitsfiles = []
+for file in glob.glob(FolderIN+"*.fits"):
+	fitsfiles.append(file)
+	IJ.open(file)
+	stackOriginal = IJ.getImage()
+	slices_nb=stackOriginal.getNSlices();
+	OriginalName = stackOriginal.title
+	OUTName = OriginalName[:len(OriginalName)-5]
+	NameOUT = "Black_"+str(Date)+"_"+OUTName
+	
+
+	time.sleep(Waitfor)
+	IJ.run("IJ Robot", "order=Left_Click x_point="+str(xVal)+" y_point="+str(yVal)+" delay=50 keypress=[]")  ## GET AVG
+	time.sleep(Waitfor)
+
+
+ 	IJ.renameResults("Time Trace(s)", "Results") 
+	Results2 = ResultsTable.getResultsTable()
+	AVG = Results2.getColumn(Results2.getColumnIndex("Average"))
+	Results = ResultsTable() 
+
+	for i in range(len(AVG)):
+		Results.incrementCounter()
+		Results.addValue('Mean', AVG[i])
+
+	Results.show('Mean')
+	path= FolderOUT+NameOUT+".txt"
+	Results.saveAs(path)
+	stackOriginal.close()
+	IJ.run("Close All", "");
+	
+	
+rm = RoiManager.getInstance()
+rm.runCommand("deselect")
+rm.runCommand("Delete")
